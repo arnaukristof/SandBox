5bdc9b70db1c75b586f40b225a50190ac9006ec7
arnaukristof <arnau.kristof12@gmail.com>
2020-02-10 18:30:50 +0100

    commit

--- src/Content.ts
+++ src/Content.ts
@@ -1,4 +1,5 @@
-﻿/* eslint-disable @typescript-eslint/no-explicit-any */
+﻿/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-inferrable-types */
 /* eslint-disable prefer-const */
 import fs from "fs";
